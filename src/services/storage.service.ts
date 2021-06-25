4f56f4d77a98bd83f965b0878a4a3dfdf8c29843
sametcelikbicak <sametcelikbicak@outlook.com>
2021-06-25 15:32:21 +0300

    Refactor storage service

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
-interface IStorageService {
+@Injectable({ providedIn: 'root' })
+export class StorageService {
   /**
    * Store value to Local storage
    *
@@ -8,7 +9,10 @@ interface IStorageService {
    * @param value - Stored value
    * @returns void
    */
-  addToLocalStorage(key: string, value: any): void;
+  public addToLocalStorage(key: string, value: any): void {
+    this.addToStorage(localStorage, key, value);
+  }
+
   /**
    * Store value to Session storage
    *
@@ -16,89 +20,73 @@ interface IStorageService {
    * @param value - Stored value
    * @returns void
    */
-  addToSessionStorage(key: string, value: any): void;
+  public addToSessionStorage(key: string, value: any): void {
+    this.addToStorage(sessionStorage, key, value);
+  }
+
   /**
    * Get value from Local storage
    *
    * @param key - Stored value key
    * @returns Stored value
    */
-  getFromLocalStorage(key: string): any;
+  public getFromLocalStorage(key: string): any {
+    return this.getFromStorage(localStorage, key);
+  }
+
   /**
    * Get value from Session storage
    *
    * @param key - Stored value key
    * @returns Stored value
    */
-  getFromSessionStorage(key: string): any;
+  public getFromSessionStorage(key: string): any {
+    return this.getFromStorage(sessionStorage, key);
+  }
+
   /**
    * Remove value from Local storage
    *
    * @param key - Stored value key
    * @returns void
    */
-  removeFromLocalStorage(key: string): void;
+  public removeFromLocalStorage(key: string): void {
+    this.removeFromStorage(localStorage, key);
+  }
+
   /**
    * Remove value from Session storage
    *
    * @param key - Stored value key
    * @returns void
    */
-  removeFromSessionStorage(key: string): void;
+  public removeFromSessionStorage(key: string): void {
+    this.removeFromStorage(sessionStorage, key);
+  }
+
   /**
    * Remove all values from Local storage
    *
    * @returns void
    */
-  clearLocalStorage(): void;
+  public clearLocalStorage(): void {
+    localStorage.clear();
+  }
+
   /**
    * Remove all values from Session storage
    *
    * @returns void
    */
-  clearSessionStorage(): void;
+  public clearSessionStorage(): void {
+    sessionStorage.clear();
+  }
+
   /**
    * Remove all values from Local and Session storage
    *
    * @returns void
    */
-  clearStorage(): void;
-}
-
-@Injectable({ providedIn: 'root' })
-export class StorageService implements IStorageService {
-  public addToLocalStorage(key: string, value: any): void {
-    this.addToStorage(localStorage, key, value);
-  }
-
-  public addToSessionStorage(key: string, value: any): void {
-    this.addToStorage(sessionStorage, key, value);
-  }
-
-  public getFromLocalStorage(key: string): any {
-    return this.getFromStorage(localStorage, key);
-  }
-
-  public getFromSessionStorage(key: string): any {
-    return this.getFromStorage(sessionStorage, key);
-  }
-
-  public removeFromLocalStorage(key: string): any {
-    this.removeFromStorage(localStorage, key);
-  }
-
-  public removeFromSessionStorage(key: string): void {
-    this.removeFromStorage(sessionStorage, key);
-  }
-
-  public clearLocalStorage(): void {
-    localStorage.clear();
-  }
-
-  public clearSessionStorage(): void {
-    sessionStorage.clear();
-  }
-
   public clearStorage(): void {
     localStorage.clear();
     sessionStorage.clear();
