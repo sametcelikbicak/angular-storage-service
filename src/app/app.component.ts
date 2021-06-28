68592c2547d356caae392ba5e6eb13a036f56de3
sametcelikbicak <sametcelikbicak@outlook.com>
2021-06-28 09:00:42 +0300

    Add buttons method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component, VERSION } from '@angular/core';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
- public title = `Angular ${VERSION.major} Storage Servis Kullanımı`;
+export class AppComponent {
+  public title = `Angular ${VERSION.major} Storage Servis Kullanımı`;
+  private key = 'StorageKey';
+  private value = 'StorageValue';
+
+  constructor(private storageService: StorageService) {}
+
+  public addToLocalStorage(): void {
+    this.storageService.addToLocalStorage(this.key, this.value);
+  }
+
+  public addToSessionStorage(): void {}
+
+  public getFromLocalStorage(): any {}
+
+  public getFromSessionStorage(): any {}
+
+  public removeFromLocalStorage(): void {}
+
+  public removeFromSessionStorage(): void {}
+
+  public clearLocalStorage(): void {}
+
+  public clearSessionStorage(): void {}
+
+  public clearStorage(): void {}
 }
